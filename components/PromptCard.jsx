dead7ee7944a564ff970f5841b184e14546e87d6
0baid <obaidurrehman555@gmail.com>
2024-10-15 19:33:22 +0500

    adding profile page and api for edit and delete

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,10 +1,19 @@
 'use client'
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
 import { useSession } from 'next-auth/react'
-import { usePathname,useRouter } from 'next/navigation'
-const PromptCard = ({post,handleTagClick,handleEdit,handleDelete}) => {
+import { usePathname, useRouter } from 'next/navigation'
+const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   const router = useRouter()
+  const [copied, setCopied] = useState("")
+  const {data:session} = useSession()
+  const pathName = usePathname()
+  
+  const handlCoppy = () => {
+    setCopied(post.prompt)
+    navigator.clipboard.writeText(post.prompt)
+    setTimeout(() => setCopied(""), 3000)
+  }
   return (
     <div className='prompt_card'>
       <div className='flex justify-between items-start gap-5'>
@@ -17,11 +26,38 @@ const PromptCard = ({post,handleTagClick,handleEdit,handleDelete}) => {
             className='rounded-full object-contain'
           />
           <div className='flex flex-col'>
-            <h3>{post.tag}</h3>
-            <p>{post.prompt}</p>
+            <h3 className='font-satoshi font-semibold text-gray-900'>{post.creator.username}</h3>
+            <p className='font-inter text-sm text-gray-500'>{post.creator.email}</p>
           </div>
         </div>
+        <div className='coppy_btn' onClick={handlCoppy}>
+          <Image
+            src={copied == post.prompt ? 'assets/icons/tick.svg' : 'assets/icons/copy.svg'}
+            alt='copy_btn'
+            width={24}
+            height={24}
+          />
+        </div>
       </div>
+      <p className='my-4 font-satoshi text-sm text-gray-700'>
+        {post.prompt}
+      </p>
+      <p className='font-inter text-sm blue_gradient cursor-pointer' onClick={() => handleTagClick && handleTagClick(post.tag)}>{post.tag}</p>
+
+
+      {session?.user.id === post.creator._id && pathName === '/profile' && (
+        <div className='mt-5 flex-center gap-4 border-t border-gray-100 pt-3'>
+          <p
+            className='font-inter text-sm green_gradient cursor-pointer'
+            onClick={handleEdit}
+          >Edit</p>
+          <p
+            className='font-inter text-sm orange_gradient cursor-pointer'
+            onClick={handleDelete}>
+            Delete
+          </p>
+        </div>
+      )}
     </div>
   )
 }
